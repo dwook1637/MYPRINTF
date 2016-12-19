38fc70bb9f1e7b3b1cd359ec92bd14f9e12a6bc9
dwook <dwook@naver.com>
2016-12-20 03:22:09 +0900

    Output of my_printf

--- my_printf.c
+++ my_printf.c
@@ -8,22 +8,51 @@ void my_printf_char(char c)
 
 void my_printf_str(char *str)
 {
-	//%s 출력
+	int i;
+
+	if (str)
+		for (i = 0; str[i]; i++)
+			my_printf_char(str[i]);
 }
 
-void my_printf_hexa(unsigned int n, char *hexa_num)
+void my_printf_hexa(unsigned int n, char *hexa_arr)
 {
-	//%x 출력
+	unsigned int cur;
+	unsigned int res;
+
+	cur = n % 16;		//cur은 0부터 15까지
+	res = (n - cur) / 16;
+	if (res)
+		my_printf_hexa(res, hexa_arr);
+	my_printf_char(hexa_arr[cur]);
 }
 
 void my_printf_int(int n)
 {
-	//%d 출력
+	int cur;		//10진수로 출력할 1의 자리 부분
+	int res;		//cur을 제외한 출력할 수의 나머지 부분
+	char c;			//아스키코드로 보정
+
+	cur = n % 10;
+	c = '0' + cur;
+	res = (n - cur) / 10;
+	if (res)		//출력할 부분이 남아있으면 계속 출력(먼저 출력됨)
+		my_printf_int(res);
+	my_printf_char(c);
 }
 
 void my_printf_octa(unsigned int n)
 {
-	//%o 출력
+	int cur;		
+	int res;		
+	char c;			//아스키코드로 보정
+
+	cur = n % 8;
+	c = '0' + cur;
+	res = (n - cur) / 8;
+	if (res)		//출력할 부분이 남아있으면 계속 출력(먼저 출력됨)
+		my_printf_int(res);
+	my_printf_char(c);
 }
 
 void my_printf(char *str, ...)			//첫번째 인자는 문자열, 그 이후 인자는 가변인자
@@ -42,7 +71,7 @@ void my_printf(char *str, ...)			//첫번째 인자는 문자열, 그 이후 인자는 가변인자
 				my_printf_int(va_arg(va, int));			
 			else if (str[i + 1] == 'c')				//%c 자리에 오는 char 문자형 출력
 				my_printf_char((char)va_arg(va, int));		
-			else if (str[i + 1] == 'x' || str[i + 1] == 'X')	//%x 자리에 오는 int형 정수 전달
+			else if (str[i + 1] == 'x')	//%x 자리에 오는 int형 정수 전달
 				my_printf_hexa(va_arg(va, unsigned int), "0123456789abcdef");
 			else if (str[i + 1] == 'o')				//%o 자리에 오는 int형 정수 전달				
 				my_printf_octa(va_arg(va, unsigned int));				
